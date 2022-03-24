1b35ebfd241f0f5a6e21372f8b6d861cd850dbd1
Matthew Chon <matthewchon@icloud.com>
2022-03-23 23:25:23 -0400

    bug fixed (image not showing)

--- src/data/data.js
+++ src/data/data.js
@@ -2,20 +2,20 @@ export const featuredPortfolio = [
   {
     id: 1,
     title: "PetFinder MapQuest",
-    img: "",
+    img: require("../images/PetFinder_Mapquest.png"),
     url: "https://github.com/MatthewChon/petfinder_mapquest",
   },
   {
     id: 2,
     title:"Lexical Analyzer",
-    img: "",
+    img: require("../images/LexicalAnalyzer.png"),
     url: "https://github.com/MatthewChon/Principle_of_ProgrammingLanguage/tree/main/assignment1",
   },
   {
     id: 3,
     title: "Computer Vision",
-    img: "",
-    url: "https://github.com/MatthewChon/CS381-ComputerVision",
+    img: require("../images/ComputerVision.png"),
+    url: "https://github.com/MatthewChon/Computer-Vision",
   }
 ];
 export const webPortfolio = [];
