2b2c6317fc06be7e63870251d2b2e047342751d3
Matthew Chon <matthewchon@icloud.com>
2022-03-24 01:25:54 -0400

    update projects

--- src/data/data.js
+++ src/data/data.js
@@ -16,6 +16,12 @@ export const Portfolio = [
     title: "Computer Vision",
     img: require("../images/ComputerVision.png"),
     url: "https://github.com/MatthewChon/Computer-Vision",
+  },
+  {
+    id:4,
+    title: "Modeling and Simulation",
+    img: require("../images/ModelingSimulation.jpeg"),
+    url: "https://github.com/MatthewChon/Simulation_And_Modelling",
   }
 ];
 export const webPortfolio = [
@@ -29,14 +35,26 @@ export const webPortfolio = [
 export const coursePortfolio = [
   {
     id: 1,
+    title: "PetFinder MapQuest",
+    img: require("../images/PetFinder_Mapquest.png"),
+    url: "https://github.com/MatthewChon/petfinder_mapquest",
+  },
+  {
+    id: 2,
     title:"Lexical Analyzer",
     img: require("../images/LexicalAnalyzer.png"),
     url: "https://github.com/MatthewChon/Principle_of_ProgrammingLanguage/tree/main/assignment1",
   },
   {
-    id: 2,
+    id: 3,
     title: "Computer Vision",
     img: require("../images/ComputerVision.png"),
     url: "https://github.com/MatthewChon/Computer-Vision",
+  },
+  {
+    id:4,
+    title: "Modeling and Simulation",
+    img: require("../images/ModelingSimulation.jpeg"),
+    url: "https://github.com/MatthewChon/Simulation_And_Modelling",
   }
 ];
